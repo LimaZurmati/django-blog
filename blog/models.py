fa9d0e520a860481954b326f0ac29d75d8d92ac7
LimaZurmati <urzalalima@gmail.com>
2024-05-21 18:15:07 +0000

    Add meta class

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 class Post(models.Model):
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -23,3 +29,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
